d196235970140eb069da77bced8a011a47bc2faf
THANHPHAM <thanhpham@users.noreply.github.com>
2026-07-30 09:10:57 +0700

    fix: change export default to module.exports to resolve Vercel Node.js syntax error

--- api/search.js
+++ api/search.js
@@ -1,6 +1,6 @@
 const yts = require('yt-search');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     // 1. Cấu hình CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
